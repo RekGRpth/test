29b6384ddae710bec226835cc349f0dcaac96947
RekGRpth <rekgrpth@gmail.com>
2021-08-31 09:11:19 +0500

    up

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -eux
 
 apk add --no-cache \
+    curl \
+    linux-headers \
+    make \
+    musl-dev \
     perl-dev \
-    perl-utils; \
+    perl-utils \
+; \
 cpan -Ti Test::Nginx::Socket Test::File; \
 echo done
