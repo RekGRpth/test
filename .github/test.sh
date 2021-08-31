b8dd2ef0683d61c0336662a9e4614052ef75d574
RekGRpth <rekgrpth@gmail.com>
2021-08-31 09:04:38 +0500

    up

--- .github/test.sh
+++ .github/test.sh
@@ -4,9 +4,7 @@ docker run \
     --env LANG=ru_RU.UTF-8 \
     --env TZ=Asia/Yekaterinburg \
     --hostname nginx \
-    --interactive \
-    --mount type=bind,source=test.conf,destination=/home/test.sh,readonly \
+    --mount type=bind,source=test.sh,destination=/home/test.sh,readonly \
     --name nginx \
     --rm \
-    --tty \
     ghcr.io/rekgrpth/nginx.docker test.sh
